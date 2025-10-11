b61aa117f8730855211137ef40227334bcd58ecf
Rutagengwa Eric <rutagebanke@gmail.com>
2025-10-11 13:06:54 +0200

    citizen model functions

--- app/Models/Citizen.php
+++ app/Models/Citizen.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Citizen extends Model
 {
-    //
+   protected $fillable = [
+        'user_id', 'national_id', 'full_name', 'date_of_birth', 
+        'address', 'phone_number', 'verification_status', 'verification_notes', 'verified_by'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function verifier()
+    {
+        return $this->belongsTo(User::class, 'verified_by');
+    }
 }
